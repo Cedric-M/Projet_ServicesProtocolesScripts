1f350edff1fa685dec23a2870af2e59a68c175ef
root <root@debian>
2016-12-06 16:32:01 +0100

    Signed-off-by: root <root@debian>
    Modifié et testé le script d'ajout de domaine pour qu'elle marche reellement sur une machine reelle

--- creerdomainePAC.sh
+++ creerdomainePAC.sh
@@ -1,24 +1,25 @@
 #lire les variables
 echo "entrez le nom du site"
-read sitename
+read sitename;
 echo "entrez l'url du site"
-read siteurl
+read siteurl;
 
 #Creer le dossier du site
-cp -r /var/www/html /var/www/$sitename
+cp -r /var/www/html /var/www/$sitename/;
 
 
 #Copier le fin du fichier de config du site par defaut vers le config du site a creer
-tail -n 18 /etc/apache2/sites-enabled/000-default.conf > /etc/apache2/sites-enabled/$sitename
+head -n 1  /etc/apache2/sites-enabled/000-default.conf >  "/etc/apache2/sites-enabled/$sitename";
+tail -n 18 /etc/apache2/sites-enabled/000-default.conf >> "/etc/apache2/sites-enabled/$sitename";
 
 #Ajouter tout l'info nessesaire
-"	ServerName $siteurl"             >> /etc/apache2/sites-enabled/$sitename
-"	ServerAdmin admin@$siteurl"      >> /etc/apache2/sites-enabled/$sitename
-"	DocumentRoot /var/www/$sitename" >> /etc/apache2/sites-enabled/$sitename
+echo "	ServerName $siteurl" >> "/etc/apache2/sites-enabled/$sitename";
+echo "	ServerAdmin admin@$siteurl" >> "/etc/apache2/sites-enabled/$sitename";
+echo "	DocumentRoot /var/www/$sitename" >> "/etc/apache2/sites-enabled/$sitename";
 
 
 #Ajouter le site a la fin du fichier de configuration de bind
-echo "zone  \"$siteurl\" { "           >> /etc/bind/named.conf
-echo "       type master;    "         >> /etc/bind/named.conf
-echo "       file \"$siteurl.zone\ ";" >> /etc/bind/named.conf
-echo "};"                              >> /etc/bind/named.conf
+echo "zone  \"$siteurl\" { "           >> /etc/bind/named.conf.local;
+echo "       type master;    "         >> /etc/bind/named.conf.local;
+echo "       file \"$siteurl.zone\ "   >> /etc/bind/named.conf.local;
+echo "};"                              >> /etc/bind/named.conf.local;
