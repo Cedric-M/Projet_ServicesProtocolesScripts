e6849a02690cab686669d0323fdc41ee4f7a5b72
root <root@debian>
2016-12-07 09:50:16 +0100

    Signed-off-by: root <root@debian>
    Decommenté la ligne de code dangereuse

--- copydossier.sh
+++ copydossier.sh
@@ -5,17 +5,17 @@ while read -r dossiersource; do
 dossierdestination="/var/www/$dossiersource";
 if [ -e "$dossiersource/.client.visible" ]; then
     #Si le fichier ".client.visible" dans le dossier source existe, on peut la copier
-	#cp -r dossiersource dossierdestination;
+	cp -r dossiersource dossierdestination;
 	echo "SHOWN for client:'$dossierdestination' from '$dossiersource'";
 else
     #Si le fichier ".client.visible" dans le dossier source n'existe pas, on ne le copie pas. Comme on est paranoiaque, on
     # efface sur la destination; on met le message d'erreur sur silencieux avec 2>/dev/null
-    #rm "$destination path" 2> /dev/null;
+    	rm "$destination path" 2> /dev/null;
 	echo "HIDDEN for client:'$dossierdestination' from '$dossiersource'";
 fi
 done
 
 apache2ctl restart;
 
-#a invoquer avec "find -d -s $dossiersource -type d | copydossier.sh"
+#a invoquer avec "find -d $dossiersource -type d | copydossier.sh"
 
